4a141b595a9f1d8883095877c98558092b3e2f70
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-16 09:00:29 +0500

    Added New Class of Gender

---
+++ OnlineQuizClasses/UserManagement/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProjectClasses.UserMgment
+{
+    public class Gender
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Gender")]
+        public string Name { get; set; }
+    }
+}
