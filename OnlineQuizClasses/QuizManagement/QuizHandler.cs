2d827db378f658467434ba325486480575073dca
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-16 10:22:06 +0500

    Added Quiz Handler to get the Data from the Database

---
+++ OnlineQuizClasses/QuizManagement/QuizHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OnlineQuizClasses.QuizManagement
+{
+    public class QuizHandler
+    {
+        private QuizContext _context = new QuizContext();
+        public List<Quiz> GetAllQuizzes()
+        {
+            using (_context)
+            {
+                return (from v in _context.Quizzes select v).ToList();
+            }
+        }
+
+
+    }
+}
