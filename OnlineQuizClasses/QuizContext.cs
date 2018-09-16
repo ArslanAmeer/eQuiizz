4bee32e8674ded38c10406db5a88d87df4a1f88e
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-16 09:08:58 +0500

    Update Added Class Gender Role and User

--- OnlineQuizClasses/QuizContext.cs
+++ OnlineQuizClasses/QuizContext.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using OnlineQuizClasses.QuizManagement;
+using OnlineQuizClasses.UserManagement;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OnlineQuizClasses.QuizManagement;
 
 namespace OnlineQuizClasses
 {
@@ -17,5 +13,9 @@ namespace OnlineQuizClasses
 
         public DbSet<Quiz> Quizzes { get; set; }
         public DbSet<Question> Questions { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
     }
 }
