ac0791ef8fbcfe95d3d13e66b276763683a30bd8
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-16 10:19:52 +0500

    Update Added Quiz History Controller To maintain the History User Created Quize

---
+++ OnLineQuizApplication/Controllers/QuizHistoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnLineQuizApplication.Controllers
+{
+    public class QuizHistoryController : Controller
+    {
+        // GET: QuizHistory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
