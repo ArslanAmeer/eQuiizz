acdbff67d0624542c88a6d57e879984019677de6
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-18 19:58:57 +0500

    Update the data base update the logic of the quiz context

---
+++ OnlineQuizClasses/Migrations/201809181353161_UpdateQuizLogic.cs
@@ -0,0 +1,16 @@
+namespace OnlineQuizClasses.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateQuizLogic : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
