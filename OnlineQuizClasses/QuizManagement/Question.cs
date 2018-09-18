dd6258505d15d9fb50d983d25781a5088411169e
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-18 22:54:04 +0500

    Update the Question Class Association

--- OnlineQuizClasses/QuizManagement/Question.cs
+++ OnlineQuizClasses/QuizManagement/Question.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace OnlineQuizClasses.QuizManagement
 {
@@ -21,5 +16,7 @@ namespace OnlineQuizClasses.QuizManagement
         public string Option4 { get; set; }
         [Required]
         public string CorrectAnswer { get; set; }
+
+        public Quiz Quiz { get; set; }
     }
 }
