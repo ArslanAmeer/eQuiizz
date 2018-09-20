9746e3c38cc139532cad0f3f4a2f2801d0cd4099
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-20 13:09:03 +0500

    Update the User Controller Added Complete USer Registration Mechanism

--- OnLineQuizApplication/Controllers/UsersController.cs
+++ OnLineQuizApplication/Controllers/UsersController.cs
@@ -27,33 +27,53 @@ namespace OnLineQuizApplication.Controllers
         [HttpGet]
         public ActionResult Signup()
         {
+            ViewBag.GenderList = ModelHelper.ToSelectItemList(new UserHandler().GetGender());
             return View();
         }
 
         [HttpPost]
         public ActionResult Signup(FormCollection fdata, User u)
         {
-
-            long numb = DateTime.Now.Ticks;
-            int count = 0;
-            foreach (string fname in Request.Files)
+            if (ModelState.IsValid)
             {
-                HttpPostedFileBase file = Request.Files[fname];
-                if (!string.IsNullOrEmpty(file?.FileName))
+                try
                 {
-                    string url = "/Content/UserImages/" + numb + "_" + ++count + file.FileName.Substring(file.FileName.LastIndexOf(".", StringComparison.Ordinal));
-                    string path = Request.MapPath(url);
-                    file.SaveAs(path);
-                    u.ImageUrl = url;
-                }
-                else
-                {
-                    string url = "/Content/UserImages/noimage.jpg";
-                    u.ImageUrl = url;
-                }
+                    u.Gender = new Gender { Id = Convert.ToInt32(fdata["gender.Name"]) };
+                    u.Role = new Role() { Id = 2 };
+                    long numb = DateTime.Now.Ticks;
+                    int count = 0;
+                    foreach (string fname in Request.Files)
+                    {
+                        HttpPostedFileBase file = Request.Files[fname];
+                        if (!string.IsNullOrEmpty(file?.FileName))
+                        {
+                            string url = "/Content/UserImages/" + numb + "_" + ++count + file.FileName.Substring(file.FileName.LastIndexOf(".", StringComparison.Ordinal));
+                            string path = Request.MapPath(url);
+                            file.SaveAs(path);
+                            u.ImageUrl = url;
+                        }
+                        else
+                        {
+                            string url = "/Content/UserImages/noimage.jpg";
+                            u.ImageUrl = url;
+                        }
 
+                    }
+                    new UserHandler().AddUser(u);
+                    return RedirectToAction("Login");
+                }
+                catch (Exception e)
+                {
+                    return RedirectToAction("ErrorLog", "Users" + e);
+                }
             }
 
+            return RedirectToAction("ErrorLog", "Users");
+
+        }
+
+        public ActionResult ErrorLog()
+        {
             return View();
         }
     }
