851c80c597a8e2fd656e1ad79e5a490538c02ec2
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-20 13:09:38 +0500

    Added NEw Model Helper  Class  Which list items

---
+++ OnLineQuizApplication/Models/ModelHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace OnLineQuizApplication.Models
+{
+    public class ModelHelper
+    {
+        public static List<SelectListItem> ToSelectItemList(dynamic values)
+
+        {
+            List<SelectListItem> templist = null;
+            if (values != null)
+            {
+                templist = new List<SelectListItem>();
+                foreach (var v in values)
+                {
+                    templist.Add(new SelectListItem { Text = v.Name, Value = Convert.ToString(v.Id) });
+                }
+                templist.TrimExcess();
+            }
+            return templist;
+        }
+    }
+}
