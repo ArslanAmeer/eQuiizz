47ab50a9a6c5850552d4db66e778f28cb0adb2d9
ArslanAmeer <arslan.ameer@hotmail.com>
2018-09-21 20:39:01 +0500

    Udpate the LoginViewModel Added Some Additional Constraints

--- OnLineQuizApplication/Models/LoginViewModel.cs
+++ OnLineQuizApplication/Models/LoginViewModel.cs
@@ -1,7 +1,19 @@
-﻿namespace OnLineQuizApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnLineQuizApplication.Models
 {
     public class LoginViewModel
     {
-
+        [Required]
+        [Display(Name = "Login Id")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Email Not Varified !!")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Please Enter More The 6 Char")]
+        public string Password { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool Rememberme { get; set; }
     }
 }
